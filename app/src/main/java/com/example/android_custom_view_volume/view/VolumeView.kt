61deaedb54e932356939c90f3b4780e50e030fa7
corgipower <greensborodogtraining@gmail.com>
2019-08-22 19:08:30 -0400

    got a small red circle inside a large black circle

--- app/src/main/java/com/example/android_custom_view_volume/view/VolumeView.kt
+++ app/src/main/java/com/example/android_custom_view_volume/view/VolumeView.kt
@@ -1,4 +1,82 @@
 package com.example.android_custom_view_volume.view
 
-class VolumeView {
+import android.content.Context
+import android.graphics.Canvas
+import android.graphics.Color
+import android.graphics.Paint
+import android.graphics.Rect
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.view.View
+
+class VolumeView(context: Context?, attrs: AttributeSet?) : View(context, attrs) {
+
+    private val rect = Rect()
+    private val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+
+    private var startX: Float = 0f
+    private var startY: Float = 0f
+    private var diffX: Float = 0f
+    private var diffY: Float = 0f
+
+    init {
+
+    }
+
+    override fun onTouchEvent(event: MotionEvent?): Boolean {
+        when(event?.action) {
+            MotionEvent.ACTION_DOWN -> {
+                startX = event.x
+                startY = event.y
+            }
+            MotionEvent.ACTION_UP -> {
+                diffX = event.x - startX
+                diffY = event.y - startY
+            }
+            MotionEvent.ACTION_MOVE -> {
+            }
+        }
+        invalidate()
+        return true
+    }
+
+    private fun largeCircle(canvas: Canvas?) {
+        val w = width/3f
+        val h = height/3f
+        var r = 0f
+
+        if (w >= h) {
+            r = w
+        } else {
+            r = h
+        }
+
+        paint.setColor(Color.BLACK)
+
+        canvas?.drawCircle(width/2f, height/2f, r, paint)
+
+    }
+
+    private fun smallCircle(canvas: Canvas?) {
+
+        val w = width/15f
+        val h = height/15f
+        var r = 0f
+
+        if (w >= h) {
+            r = w
+        } else {
+            r = h
+        }
+
+        paint.setColor(Color.RED)
+
+        canvas?.drawCircle(width/2f, height/2f, r, paint)
+    }
+
+    override fun onDraw(canvas: Canvas?) {
+        super.onDraw(canvas)
+        largeCircle(canvas)
+        smallCircle(canvas)
+    }
 }
